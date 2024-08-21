e0f276e5d26dbae67dc43dfab3aec2ce6089e962
ehancup <mondokskuy19@gmail.com>
2024-08-21 10:59:20 +0700

    add video

--- src/page/home.jsx
+++ src/page/home.jsx
@@ -35,11 +35,11 @@ function Home() {
   };
 
   useEffect(() => {
-    window.addEventListener('load' , () => {
-    setTimeout(() => {
-      loadDone();
-    }, 3000);
-    })
+    window.addEventListener("load", () => {
+      setTimeout(() => {
+        loadDone();
+      }, 3000);
+    });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
@@ -67,10 +67,10 @@ function Home() {
         </div>
         <div className="">
           <Section3 backgroundColor={backgroundColor} setop={setop} />
-          <Section4 backgroundColor={backgroundColor} setop={setop} />
-          <Trippy />
-          <Horizontal />
-          <Highlight />
+          <Section4 backgroundColor={backgroundColor} />
+          <Trippy setop={setop} />
+          <Horizontal backgroundColor={backgroundColor} setop={setop} />
+          <Highlight backgroundColor={backgroundColor} />
           {/* <div className="h-screen"></div> */}
         </div>
       </div>
@@ -90,14 +90,20 @@ function Section1({ scrollYProgress }) {
 
   useEffect(() => {
     setTimeout(() => {
-      conRef.current.classList.remove('transition-all', 'duration-1000', 'ease-out')
-    }, 1000)
-  })
+      conRef.current.classList.remove(
+        "transition-all",
+        "duration-1000",
+        "ease-out"
+      );
+    }, 1000);
+  });
   return (
     <motion.section
       style={{ scale, rotate }}
       ref={conRef}
-      className={`${isLoading ? 'h-[300vh] ' : 'h-screen'} transition-all duration-1000 ease-out  w-full  sticky top-0`}
+      className={`${
+        isLoading ? "h-[300vh] " : "h-screen"
+      } transition-all duration-1000 ease-out  w-full  sticky top-0`}
     >
       <div className=" relative bottom-0 w-fit h-full left-1/2 -translate-x-1/2">
         <motion.img
@@ -139,7 +145,7 @@ function Section1({ scrollYProgress }) {
   );
 }
 const JIKON =
-  "Vocational School student majoring in Software Engineering. Individuals who are quick to learn, and reliable, Master several programming languages ​​and techniques in developing web-based or mobile applications.";
+  "High school student in Software Engineering with a passion for web and mobile app development. Eager to learn and tackle new challenges in tech.";
 function Section2({ scrollYProgress }) {
   const containerRef = useRef(null);
   const { scrollYProgress: containerProgress } = useScroll({
@@ -197,7 +203,7 @@ function Section2({ scrollYProgress }) {
             </div>
           </div>
         </motion.div>
-        <div className="flex-1 flex flex-col">
+        <div className="flex-1 flex flex-col gap-3">
           <motion.div
             style={{ translateY: titleTransform }}
             className="w-fit h-fit relative"
@@ -207,13 +213,13 @@ function Section2({ scrollYProgress }) {
               className="w-full scale-x-50 h-full bg-whiteal absolute origin-left  "
             ></motion.div>
 
-            <h1 className=" text-4xl font-roslindale-reg text-greyal  w-fit mx-3 relative ">
+            <h1 className=" text-5xl font-roslindale-reg text-greyal  w-fit mx-3 relative ">
               Raihan Yusuf Firmansyah
             </h1>
           </motion.div>
           <motion.div
             style={{ translateY: paragraphTransform }}
-            className="flex flex-row flex-wrap items-center gap-x-3 justify-start text-whiteal text-5xl font-bodoni mt-5"
+            className="flex flex-row flex-wrap items-center gap-x-3 gap-y-1 justify-start text-whiteal text-5xl font-bodoni mt-5"
           >
             {words.map((word, i) => {
               const start = i / words.length;
@@ -236,7 +242,7 @@ function Section2({ scrollYProgress }) {
 }
 
 const CODING =
-  "<h1>Fatum Brutum Amor Fati</h1> tai dolor sit amet consectetur adipisicing elit. Ex quod cumque, itaque distinctio dicta deleniti voluptas, non porro inventore ducimus nihil doloremque, ipsam odio. Laudantium necessitatibus atque esse dicta ad.";
+  "I can code in several languages adn master several framework. So, just contact me if U need something";
 
 function Section3({ backgroundColor, setop }) {
   const ref = useRef(null);
@@ -282,36 +288,16 @@ function Section3({ backgroundColor, setop }) {
     return loadedImages;
   }, []);
 
-  const currentIndex = useTransform(carouselProgress, [0, 1], [1, 157]);
+  const currentIndex = useTransform(carouselProgress, [0, 0.8], [1, 157]);
+  const transEl = useTransform(carouselProgress, [0.8, 0.95], ["100%", "0%"]);
+  const transP = useTransform(carouselProgress, [0.8, 0.95], [500, 0]);
   const render = useCallback(
     (index) => {
       if (images[index - 1]) {
-        const canvasRatio = canvasRef.current.width / canvasRef.current.height;
-        const imageRatio = images[index - 1].width / images[index - 1].height;
         canvasRef.current.imageSmoothingEnabled = true;
 
         canvasRef.current.width = images[index - 1].width;
         canvasRef.current.height = images[index - 1].height;
-
-        console.log(imageRatio);
-        console.log(canvasRatio);
-
-        let drawWidth, drawHeight, offsetX, offsetY;
-
-        // Sesuaikan ukuran gambar berdasarkan rasio aspek
-        if (imageRatio > canvasRatio) {
-          // Gambar lebih lebar dari kanvas
-          drawWidth = canvasRef.current.width;
-          drawHeight = canvasRef.current.width / imageRatio;
-          offsetX = 0;
-          offsetY = (canvasRef.current.height - drawHeight) / 2;
-        } else {
-          // Gambar lebih tinggi dari kanvas
-          drawWidth = canvasRef.current.height * imageRatio;
-          drawHeight = canvasRef.current.height;
-          offsetX = (canvasRef.current.width - drawWidth) / 2;
-          offsetY = 0;
-        }
 
         canvasRef.current
           ?.getContext("2d")
@@ -360,8 +346,11 @@ function Section3({ backgroundColor, setop }) {
   // console.log(rilImg);
 
   const icon = [
+    "/icon/html-grey.svg",
+    "/icon/css-grey.svg",
     "/icon/javascript-grey.svg",
     "/icon/typescript-grey.svg",
+    "/icon/tailwind-grey.svg",
     "/icon/react-grey.svg",
     "/icon/nextjs-grey.svg",
     "/icon/nestjs-grey.svg",
@@ -371,6 +360,7 @@ function Section3({ backgroundColor, setop }) {
     "/icon/mongodb-grey.svg",
     "/icon/firebase-grey.svg",
     "/icon/docker-grey.svg",
+    "/icon/unity-grey.svg",
   ];
 
   return (
@@ -383,8 +373,12 @@ function Section3({ backgroundColor, setop }) {
         className="h-screen sticky top-0 flex flex-row items-center px-44 gap-20"
         ref={ref}
       >
+        <motion.div
+          style={{ scaleX: carouselProgress }}
+          className=" origin-left absolute w-full top-0 h-2 bg-whiteal inset-x-0"
+        ></motion.div>
         <div className="flex-1 flex flex-col">
-          <div className="flex flex-row flex-wrap items-center gap-x-3 justify-start text-whiteal text-3xl font-bodoni">
+          <div className="flex flex-row flex-wrap items-center gap-x-3 gap-y-2 justify-start text-whiteal text-5xl font-bodoni">
             {words.map((word, i) => {
               const start = i / words.length;
               const end = start + 1 / words.length;
@@ -419,39 +413,149 @@ function Section3({ backgroundColor, setop }) {
         </div>
         <motion.div
           style={{ translateY: imageY }}
-          className="w-96 aspect-[16/24] bg-whiteal overflow-hidden rounded-xl"
+          className="w-96 aspect-[16/24] bg-whiteal overflow-hidden rounded-xl relative"
         >
           <canvas className="w-full h-full" ref={canvasRef}></canvas>
           {/* <motion.img src={currentImage} alt="" className="w-full" /> */}
+          <motion.div
+            style={{ translateY: transEl }}
+            className="absolute inset-0 bg-whiteal grid place-items-center"
+          >
+            <motion.p
+              style={{ translateY: transP }}
+              className="text-7xl font-roslindale-bold text-greyal text-center"
+            >
+              Trust <br />
+              Me!
+            </motion.p>
+          </motion.div>
         </motion.div>
       </div>
     </motion.section>
   );
 }
 
-function Section4({ backgroundColor, setop }) {
+const EDIT = "I also can edit a video for you.";
+function Section4({ backgroundColor }) {
   const ref = useRef(null);
+  const carouselRef = useRef(null);
+  const canvasRef = useRef(null);
+  const { scrollYProgress: carouselProgress } = useScroll({
+    target: carouselRef,
+    offset: ["start start", "end end"],
+  });
   const { scrollYProgress } = useScroll({
     target: ref,
-    offset: ["end end", "end center"],
+    offset: ["start end", "start start"],
+  });
+  const { scrollYProgress: transP } = useScroll({
+    target: ref,
+    offset: ["start end", "end start"],
   });
 
-  useEffect(() => {
-    scrollYProgress.on("change", (e) => setop(1 - e));
-  });
+  const words = EDIT.split(" ");
+  // useEffect(() => {
+  //   scrollYProgress.on("change", (e) => setop(1 - e));
+  // });
+  const images = useMemo(() => {
+    const loadedImages = [];
+
+    for (let i = 1; i <= 94; i++) {
+      const img = new Image();
+      img.src = `/edit/${i}.webp`;
+      img.className = "w-full h-full object-cover";
+      loadedImages.push(img);
+    }
+
+    return loadedImages;
+  }, []);
 
   // const backgroundColor = useTransform(scrollYProgress, [0,1], ["#252222", "#f7f7ef"])
+  const transImage = useTransform(transP, [0,0.5,1], [500,0,-500])
+  const transText = useTransform(transP, [0,0.5,1], [300,0,-300])
+
+  const currentIndex = useTransform(carouselProgress, [0, 1], [1, 94]);
+
+  const render = useCallback(
+    (index) => {
+      if (images[index - 1]) {
+        canvasRef.current.imageSmoothingEnabled = true;
+
+        canvasRef.current.width = images[index - 1].width;
+        canvasRef.current.height = images[index - 1].height;
+
+        canvasRef.current
+          ?.getContext("2d")
+          ?.drawImage(
+            images[index - 1],
+            0,
+            0,
+            canvasRef.current.width,
+            canvasRef.current.height
+          );
+      }
+    },
+    [images]
+  );
+
+  useEffect(() => {
+    const img = new Image();
+    img.src = `/edit/1.webp`;
+    img.className = "w-full h-full object-cover";
+
+    canvasRef.current.width = img.width;
+    canvasRef.current.height = img.height;
+
+    canvasRef.current
+      ?.getContext("2d")
+      ?.drawImage(img, 0, 0, canvasRef.current.width, canvasRef.current.height);
+  }, []);
+
+  useEffect(() => {
+    currentIndex.on("change", (latest) => {
+      render(Number(latest.toFixed()));
+      // setImage(`/coding/${Math.floor(latest)}.webp`);
+      // indexImage.set(Math.floor(latest))
+    });
+  }, [currentIndex, render]);
   return (
-    <motion.section
-      style={{ backgroundColor }}
-      ref={ref}
-      className="h-screen w-full"
-    >
-      <p className="text-5xl font-bold">test section</p>
+    <motion.section className="h-[500vh] w-full relative bg-red-500" ref={carouselRef}>
+      <motion.div
+        style={{ backgroundColor }}
+        ref={ref}
+        className="h-screen w-full top-0 sticky grid place-items-center"
+      >
+        <motion.div
+          style={{ scaleX: carouselProgress }}
+          className=" origin-right absolute w-full top-0 h-2 bg-greyal inset-x-0"
+        ></motion.div>
+        <div className=" flex flex-col w-full items-center justify-center gap-10">
+          <motion.div style={{translateY: transText}} className="flex flex-row flex-wrap items-center gap-x-3 gap-y-2 justify-start text-greyal text-7xl font-roslindale-reg">
+            {words.map((word, i) => {
+              const start = i / words.length;
+              const end = start + 1 / words.length;
+              return (
+                <Word
+                  key={i}
+                  scrollYProgress={scrollYProgress}
+                  range={[start, end]}
+                >
+                  {word}
+                </Word>
+              );
+            })}
+          </motion.div>
+          <div className="w-full px-96">
+            <motion.div style={{translateY: transImage}} className="aspect-video w-full bg-greyal rounded-xl overflow-hidden shadow-2xl">
+            <canvas className="w-full h-full" ref={canvasRef}></canvas>
+            </motion.div>
+          </div>
+        </div>
+      </motion.div>
     </motion.section>
   );
 }
-function Trippy() {
+function Trippy({setop}) {
   const trippyRef = useRef(null);
   const bgRef = useRef(null);
   const { scrollYProgress } = useScroll({
@@ -459,6 +563,9 @@ function Trippy() {
     offset: ["start start", "end end"],
     // smooth: 20
   });
+  useEffect(() => {
+    scrollYProgress.on('change', (value) => setop(1-value))
+  })
   const scale = useTransform(scrollYProgress, [0.02, 0.5, 1], [1, 50, 119]);
   const translateY = useTransform(scrollYProgress, [0.02, 1], [0, 4000]);
   const { scrollYProgress: bgYProgress } = useScroll({
@@ -468,7 +575,7 @@ function Trippy() {
   return (
     <div className="h-[600vh] bg-gray-600 z-0 relative top-0 " ref={trippyRef}>
       <div
-        className="h-screen bg-green-500 w-full sticky top-0  overflow-hidden"
+        className="h-screen bg-whiteal w-full sticky top-0  overflow-hidden"
         ref={bgRef}
       >
         <motion.div
@@ -477,7 +584,7 @@ function Trippy() {
         >
           <motion.p
             style={{ opacity: bgYProgress }}
-            className="text-8xl text-center  text-red-700 font-roslindale-bold"
+            className="text-8xl text-center  text-greyal font-roslindale-bold"
           >
             ENTER
             <br />
@@ -492,7 +599,7 @@ function Trippy() {
 const WORDS =
   "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex quod cumque, itaque distinctio dicta deleniti voluptas, non porro inventore ducimus nihil doloremque, ipsam odio. Laudantium necessitatibus atque esse dicta ad.";
 
-function Highlight() {
+function Highlight({backgroundColor}) {
   const element = useRef(null);
   const words = WORDS.split(" ");
   const { scrollYProgress } = useScroll({
@@ -503,11 +610,14 @@ function Highlight() {
     scrollYProgress.on("change", (e) => console.log(e));
   });
   return (
-    <div
-      className="h-screen w-full bg-red-700 grid place-items-center"
+    <motion.div
+    style={{
+      backgroundColor
+    }}
+      className="h-screen w-full grid place-items-center"
       ref={element}
     >
-      <div className="flex flex-row flex-wrap items-center gap-3 justify-start text-whiteal text-5xl font-bold font-bodoni">
+      <div className="flex flex-row flex-wrap items-center gap-3 justify-start text-greyal text-5xl font-bold font-bodoni">
         {words.map((word, i) => {
           const start = i / words.length;
           const end = start + 1 / words.length;
@@ -522,7 +632,7 @@ function Highlight() {
           );
         })}
       </div>
-    </div>
+    </motion.div>
   );
 }
 
@@ -564,23 +674,32 @@ function Character({ scrollYProgress, range, children }) {
   );
 }
 
-function Horizontal() {
+function Horizontal({backgroundColor, setop}) {
   const targetRef = useRef(null);
+  const carouselRef = useRef(null);
   const { scrollYProgress } = useScroll({
     target: targetRef,
     offset: ["start start", "end end"],
   });
+  const { scrollYProgress: carouselProgress } = useScroll({
+    target: targetRef,
+    offset: ["end end", "end start"],
+  });
+
+  useEffect(() => {
+    carouselProgress.on('change', (e) => setop(e))
+  })
 
   const x = useTransform(scrollYProgress, [0, 1], ["1%", "-90%"]);
   return (
     <section className="h-[400vh] w-full relative" ref={targetRef}>
-      <div className="h-screen w-full sticky top-0 flex items-center">
+      <motion.div style={{backgroundColor}} className="h-screen w-full sticky top-0 flex items-center" ref={carouselRef}>
         <motion.div style={{ x }} className="flex flex-row gap-20 flex-nowrap">
           {Array.from({ length: 10 }, (_, i) => (
             <ProjectCard key={i} />
           ))}
         </motion.div>
-      </div>
+      </motion.div>
     </section>
   );
 }
